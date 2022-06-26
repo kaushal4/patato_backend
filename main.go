e4f4f20f6980f90dca0197d5c09605ff2ce59f05
Kaushal <damaniakaushal4@gmail.com>
2022-06-26 21:09:19 +0530

    💪added jwt support

--- main.go
+++ main.go
@@ -9,9 +9,15 @@ import (
 func main() {
     port := 8080
     fmt.Println("starting server in port :",port)
-    router := gin.Default()
-    router.POST("/user",user.SignUp)
-    router.POST("/user/login",user.Login)
+    router := gin.New()
+    userRoutes := router.Group("/user")
+    {
+        userRoutes.POST("/",user.SignUp)
+        userRoutes.POST("/login",user.Login)
+        userRoutes.GET("/test",user.CheckToken,user.MiddlewareTest)
+        userRoutes.GET("/refresh",user.CheckToken,user.RefreshToken)
+
+    }
     router.Run(fmt.Sprintf("localhost:%d",port))
 }
 
--- user/user.go
+++ user/user.go
@@ -1,12 +1,15 @@
 package user
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"potato/backend/db"
 	"potato/backend/utils"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -43,6 +46,14 @@ func SignUp(c *gin.Context){
     c.JSON(http.StatusCreated,gin.H{"status":"Insertion Successfull"})
 }
 
+var jwtKey = []byte("my_secret_key")
+
+
+type Claims struct {
+	Id string `json:"id"`
+	jwt.StandardClaims
+}
+
 
 func Login(c *gin.Context){
     var user User
@@ -64,5 +75,93 @@ func Login(c *gin.Context){
     if utils.CheckError(err,http.StatusUnauthorized,c){
         return
     }
-    c.JSON(http.StatusAccepted,gin.H{"status":"Login successfull"})
+    expirationTime := time.Now().Add(5 * time.Hour)
+    claims := &Claims{
+        Id: string(dbUser.Id),
+        StandardClaims: jwt.StandardClaims{
+            // In JWT, the expiry time is expressed as unix milliseconds
+            ExpiresAt: expirationTime.Unix(),
+        },
+    }
+    // Declare the token with the algorithm used for signing, and the claims
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    // Create the JWT string
+    tokenString, err := token.SignedString(jwtKey)
+    if utils.CheckError(err,http.StatusUnauthorized,c){
+        return
+    }
+    c.SetCookie("token",tokenString,int(5*60*60),"/","localhost",false,false)
+    c.JSON(http.StatusAccepted,gin.H{"status":"Login successfull","token":tokenString})
+}
+
+func CheckToken(c *gin.Context){
+    token,err := c.Cookie("token")
+    if err != nil {
+        if err == http.ErrNoCookie {
+            c.AbortWithStatus(http.StatusUnauthorized)
+            return
+        }
+        c.AbortWithStatus(http.StatusBadRequest)
+        return
+    }
+    var claims Claims
+    tkn, err := jwt.ParseWithClaims(token, &claims, func(token *jwt.Token) (interface{}, error) {
+        return jwtKey, nil
+    })
+
+    if err != nil {
+        fmt.Println(err)
+        if err == jwt.ErrSignatureInvalid {
+            c.AbortWithStatus(http.StatusUnauthorized)
+            return
+        }
+        c.AbortWithStatus(http.StatusBadRequest)
+        return
+    }
+    if !tkn.Valid {
+        c.AbortWithStatus(http.StatusUnauthorized)
+        return
+    }
+    c.Set("claims",claims)
+    c.Next()
+}
+
+func checkClaims(c* gin.Context) (Claims,error) {
+    var claims any
+    var isPresent bool
+    if claims,isPresent = c.Get("claims");!isPresent{
+        c.Status(http.StatusUnauthorized)
+        return Claims{},errors.New("No Claims")
+    }
+    _,ok := claims.(Claims)
+    if !ok{
+        c.Status(http.StatusUnauthorized)
+        return Claims{},errors.New("No Claims")
+    }
+    return claims.(Claims),nil
+}
+
+func MiddlewareTest(c * gin.Context){
+    claims,err := checkClaims(c)
+    if utils.CheckError(err,http.StatusInternalServerError,c){
+        return
+    }
+    c.JSON(http.StatusOK,gin.H{"status":"works!","id":claims.Id})
+}
+
+func RefreshToken(c * gin.Context){
+    claims,err := checkClaims(c)
+    if utils.CheckError(err,http.StatusInternalServerError,c){
+        return
+    }
+    expirationTime := time.Now().Add(5 * time.Minute)
+    claims.ExpiresAt = expirationTime.Unix()
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    tokenString, err := token.SignedString(jwtKey)
+    if err != nil {
+        c.Status(http.StatusInternalServerError)
+        return
+    }
+    c.SetCookie("token",tokenString,int(5*60*60),"/","localhost",false,false)
+    c.JSON(http.StatusAccepted,gin.H{"status":"Refreshed","token":tokenString})
 }
